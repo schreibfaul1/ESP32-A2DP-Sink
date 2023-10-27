3560cb8d95cae4b8632a4b23007320f3e876d050
Wolle <schreibfaul1@t-online.de>
2023-10-27 19:33:01 +0200

    Update ESP32_BT_sink.ino

--- ESP32_BT_sink.ino
+++ ESP32_BT_sink.ino
@@ -1,4 +1,3 @@
-
 #include <Arduino.h>
 
 #include "a2dp_sink.h"
@@ -13,10 +12,19 @@ char BT_SINK_NAME[]   = "ESP32-SPEAKER"; // sink devicename
 
 
 void setup() {
+    Serial.begin(115200);
     a2dp_sink_init(BT_SINK_NAME);
     set_i2s_pinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
 }
 
 
 void loop() {
+}
+
+void bt_info(const char* info){
+ // Serial.printf("bt info:  %s\n", info);
+}
+
+void bt_state(const char* info){
+  Serial.printf("bt state:  %s\n", info);
 }
